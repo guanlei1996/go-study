415726d3dc133e1076703f401776519144c3909d
Lay <guanlei@geeweb.cn>
2018-04-28 14:00:36 +0800

    update

---
+++ 1_variable/ConstMake.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const a  = 1
+const (
+	b = 2
+	c = 3
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
---
+++ 1_variable/VariableMake.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int
+	a = 10
+	fmt.Println(a)
+
+	b:=10
+	fmt.Println(b)
+
+	var c = 10
+	fmt.Println(c)
+}
